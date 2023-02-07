9efc747f7dc0e3e89f22109d4603cde3b228c9d4
tringapps-srinivasan <31205587+Srinivasan-TA@users.noreply.github.com>
2023-02-07 17:15:05 +0530

    initial

---
+++ src/main/java/org/example/Hashmap.java
@@ -0,0 +1,33 @@
+package org.example;
+import java.util.*;
+import java.util.logging.Logger;
+
+public class Hashmap {
+
+    public static void main(String args[]) {
+        Logger l = Logger.getLogger("com.api.jar");
+        HashMap<Integer, String> Students = new HashMap<Integer, String>();
+        l.info("The names added are " + Students);
+        Students.put(100, "Abisha");
+        Students.put(101, "Srini");
+        Students.put(102, "Ruchi");
+
+        l.info("After invoking put() method ");
+        for (Map.Entry m : Students.entrySet()) {
+            System.out.println(m.getKey() + " " + m.getValue());
+        }
+
+        Students.putIfAbsent(103, "Bargavi");
+        l.info("After invoking putIfAbsent() method ");
+        for (Map.Entry m : Students.entrySet()) {
+            System.out.println(m.getKey() + " " + m.getValue());
+        }
+        HashMap<Integer, String> map = new HashMap<Integer, String>();
+        map.put(104, "Priya");
+        map.putAll(Students);
+        l.info("After invoking putAll() method ");
+        for (Map.Entry m : map.entrySet()) {
+            l.info(m.getKey() + " " + m.getValue());
+        }
+    }
+}
---
+++ src/main/java/org/example/Treeset.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import java.util.*;
+import java.util.logging.Logger;
+
+public class Treeset {
+    public static void main(String[] args) {
+        Logger l = Logger.getLogger("com.api.jar");
+        TreeSet<String> cites=new TreeSet<String>();
+        cites.add("Tanjore");
+        cites.add("Palakad");
+        cites.add("Madurai");
+        cites.add("Chennai");
+        l.info("An initial list of Cities in TN: "+cites);
+        cites.remove("Chennai");
+        l.info("After invoking remove method: "+cites);
+        HashSet<String> set1=new HashSet<String>();
+        set1.add("Coimbatore");
+        set1.add("Trichy");
+        cites.addAll(set1);
+        l.info("Updated Cites in TN: "+cites);
+        cites.removeAll(set1);
+        l.info("After invoking removeAll() method: "+cites);
+        cites.removeIf(str->str.contains("Palakad"));
+        l.info("After invoking "+cites);
+        cites.clear();
+        l.info("After invoking clear() method: "+cites);
+    }
+
+}
