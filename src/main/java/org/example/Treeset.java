b4ea86c63cdb09fdc61eac84c6ec1b83b20129a7
tringapps-srinivasan <31205587+Srinivasan-TA@users.noreply.github.com>
2023-02-08 12:02:23 +0530

    initial

--- src/main/java/org/example/Treeset.java
+++ src/main/java/org/example/Treeset.java
@@ -7,25 +7,25 @@ import java.util.logging.Logger;
 public class Treeset {
     public static void main(String[] args) {
         Logger l = Logger.getLogger("com.api.jar");
-        TreeSet<String> cites=new TreeSet<String>();
-        cites.add("Tanjore");
-        cites.add("Palakad");
-        cites.add("Madurai");
-        cites.add("Chennai");
-        l.log(Level.INFO,()->"An initial list of Cities in TN: "+cites);
-        cites.remove("Chennai");
-        l.log(Level.INFO,()->"After invoking remove method: "+cites);
+        TreeSet<String> states=new TreeSet<String>();
+        states.add("TN");
+        states.add("Punjab");
+        states.add("MP");
+        states.add("KL");
+        l.log(Level.INFO,()->"An initial list of states in India: "+states);
+        states.remove("Chennai");
+        l.log(Level.INFO,()->"After invoking remove method: "+states);
         TreeSet<String> set1=new TreeSet<String>();
-        set1.add("Coimbatore");
-        set1.add("Trichy");
-        cites.addAll(set1);
-        l.log(Level.INFO,()->"Updated Cites in TN: "+cites);
-        cites.removeAll(set1);
-        l.log(Level.INFO,()->"After invoking removeAll() method: "+cites);
-        cites.removeIf(str->str.contains("Palakad"));
-        l.log(Level.INFO,()->"After invoking "+cites);
-        cites.clear();
-        l.log(Level.INFO,()->"After invoking clear() method: "+cites);
+        set1.add("Chatisgar");
+        set1.add("Telangana");
+        states.addAll(set1);
+        l.log(Level.INFO,()->"Updated states in TN: "+states);
+        states.removeAll(set1);
+        l.log(Level.INFO,()->"After invoking removeAll() method: "+states);
+        states.removeIf(str->str.contains("Palakad"));
+        l.log(Level.INFO,()->"After invoking "+states);
+        states.clear();
+        l.log(Level.INFO,()->"After invoking clear() method: "+states);
     }
 
 }
