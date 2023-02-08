9e492c574505ac62f133ab4940f1960df9bac305
tringapps-srinivasan <31205587+Srinivasan-TA@users.noreply.github.com>
2023-02-08 10:21:18 +0530

    initial

--- src/main/java/org/example/Hashmap.java
+++ src/main/java/org/example/Hashmap.java
@@ -6,24 +6,24 @@ public class Hashmap {
 
     public static void main(String[] args) {
         Logger l = Logger.getLogger("com.api.jar");
-        HashMap<Integer, String> Students = new HashMap<Integer, String>();
-        l.info("The names added are " + Students);
-        Students.put(100, "Abisha");
-        Students.put(101, "Srini");
-        Students.put(102, "Ruchi");
+        HashMap<Integer, String> students = new HashMap<Integer, String>();
+        l.info("The names added are " + students);
+        students.put(100, "Abisha");
+        students.put(101, "Srini");
+        students.put(102, "Ruchi");
         l.info("After invoking put() method ");
-        for (Map.Entry m : Students.entrySet()) {
+        for (Map.Entry m : students.entrySet()) {
             l.info(m.getKey() + " " + m.getValue());
         }
 
-        Students.putIfAbsent(103, "Bargavi");
+        students.putIfAbsent(103, "Bargavi");
         l.info("After invoking putIfAbsent() method ");
-        for (Map.Entry m : Students.entrySet()) {
+        for (Map.Entry m : students.entrySet()) {
             l.info(m.getKey() + " " + m.getValue());
         }
         HashMap<Integer, String> map = new HashMap<Integer, String>();
         map.put(104, "Priya");
-        map.putAll(Students);
+        map.putAll(students);
         l.info("After invoking putAll() method ");
         for (Map.Entry m : map.entrySet()) {
             l.info(m.getKey() + " " + m.getValue());
